c9336c47d90f07d9163a649936dad0c6ea04966a
andrewmcodes <andrew.mason@n2pub.com>
2018-09-14 21:18:50 -0400

    do a few ui things

--- app/javascript/bundles/Welcome/components/Welcome.jsx
+++ app/javascript/bundles/Welcome/components/Welcome.jsx
@@ -11,26 +11,23 @@ export default class Welcome extends React.Component {
     this.state = { name: this.props.name };
   }
 
-  updateName = (name) => {
+  updateName = name => {
     this.setState({ name });
   };
 
   render() {
     return (
       <div>
-        <h1
-          className="mb-4"
-        >
-          Hello, {this.state.name}!
-        </h1>
+        <h1 className="mb-8 text-purple">Enter your dynamic message below!</h1>
+        <p className="mb-4 max-w-full">Message: {this.state.name}</p>
         <hr />
-        <form >
+        <form>
           <input
             id="name"
-            className="bg-blue-lightest p-2 text-grey-light"
+            className="bg-blue-lightest p-2 text-grey-light w-full"
             type="text"
             value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
+            onChange={e => this.updateName(e.target.value)}
           />
         </form>
       </div>
